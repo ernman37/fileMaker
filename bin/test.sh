d9dab1be4dc3d275d7a22ceea9d8a404306452c9
ernman37 <Rion.duckworth@gmail.com>
2022-02-06 02:45:39 -0700

    tests

--- bin/test.sh
+++ bin/test.sh
@@ -6,16 +6,38 @@
 
 PROGRAM="/Users/rionduckworth/codeProjects/bash/fileMaker/src/fileMaker.sh"
 FILENAME="example"
+TESTDIR="testDir"
+OPTIONS=( "-c" "-C" "-ch" "-Ch" "-j" "-p" "-m" "-b")
 
 function main() {
-   mkdir "test"
-   cd "test"
-   $PROGRAM "-ch" $FILENAME 
-   $PROGRAM "-Ch" $FILENAME 
-   $PROGRAM "-j" $FILENAME 
-   $PROGRAM "-p" $FILENAME 
-   $PROGRAM "-m" $FILENAME 
-   $PROGRAM "-b" $FILENAME 
+   if [[ ! -d "$TESTDIR" ]];
+   then
+      mkdir "$TESTDIR"
+   fi
+   cd "$TESTDIR"
+   if [[ $# == 0 ]];
+   then
+      testBuildAllValid
+   else
+      cd ..
+      rm -r "$TESTDIR"
+   fi
 }
 
-main
+function testBuildAllValid() {
+   for arg in "${OPTIONS[@]}"
+   do
+      testProgram "$arg" 
+   done
+}
+
+function testProgram() {
+   local testFile="$FILENAME"
+   if [[ $# == 2 ]];
+   then
+      testFile="$2"
+   fi
+   $PROGRAM "$1" "$testFile"
+}
+
+main $@
