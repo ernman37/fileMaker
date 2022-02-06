337140c434b6919f575a99f51d1cdcd8b122daad
ernman37 <Rion.duckworth@gmail.com>
2022-02-06 02:56:31 -0700

    tests

--- bin/test.sh
+++ bin/test.sh
@@ -4,27 +4,37 @@
 #   For: fileMaker
 #   Description: Tests to make sure that fileMaker is working correctly
 
+#Color for Output 
+NO='\033[0m'
+R='\033[0;31m'
+G='\033[0;32m'
+O='\033[0;33m'
+P='\033[0;35m'
+B='\033[0;36m'
+
 PROGRAM="/Users/rionduckworth/codeProjects/bash/fileMaker/src/fileMaker.sh"
 FILENAME="example"
 TESTDIR="testDir"
-OPTIONS=( "-c" "-C" "-ch" "-Ch" "-j" "-p" "-m" "-b")
+OPTIONS=( "-ch" "-Ch" "-c" "-C" "-j" "-p" "-m" "-b")
 
 function main() {
+   deleteIfDirExists
+   testBuildAllValid
+}
+
+function deleteIfDirExists() {
    if [[ ! -d "$TESTDIR" ]];
    then
       mkdir "$TESTDIR"
-   fi
-   cd "$TESTDIR"
-   if [[ $# == 0 ]];
-   then
-      testBuildAllValid
+      cd "$TESTDIR"
    else
-      cd ..
-      rm -r "$TESTDIR"
+      rm -r $TESTDIR
+      exit $?
    fi
 }
 
 function testBuildAllValid() {
+   echoC $O "Testing All Valid"
    for arg in "${OPTIONS[@]}"
    do
       testProgram "$arg" 
@@ -38,6 +48,10 @@ function testProgram() {
       testFile="$2"
    fi
    $PROGRAM "$1" "$testFile"
+}
+
+function echoC() {
+   echo -e "${1}$2${NO}"
 }
 
 main $@
