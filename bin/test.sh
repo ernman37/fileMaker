2d8a6dcd70a0432e7dc18250ec0cb42ae42c952d
ernman37 <Rion.duckworth@gmail.com>
2023-02-20 23:09:54 -0700

    add new features

--- bin/test.sh
+++ bin/test.sh
@@ -15,7 +15,7 @@ B='\033[0;36m'
 PROGRAM="/Users/rionduckworth/codeProjects/bash/fileMaker/src/fileMaker.sh"
 FILENAME="example"
 TESTDIR="testDir"
-OPTIONS=( "ch" "Ch" "c" "C" "j" "p" "m" "b" "H" "s" "js" "jst" "t" "Tt" "M" "r" "P" "n" )
+OPTIONS=( "ch" "Ch" "c" "C" "j" "p" "m" "b" "H" "s" "js" "jst" "t" "Tt" "M" "r" "P" "n" "d" )
 
 function main() {
    deleteIfDirExists
